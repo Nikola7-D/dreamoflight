fb192fdc2236f8cfda39e15fe6e9343aad203c0b
zourit77 <zourit77@free.fr>
2019-11-12 15:19:09 +0100

    Add files via upload

---
+++ install-21.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+arch-chroot /mnt && echo "KEYMAP=fr-latin9 && FONT=eurlatgr" > /etc/vconsole.conf && echo "LANG=fr_FR.UTF-8 LC_COLLATE=C" > /etc/locale.conf && echo "fr_FR.UTF-8 UTF-8" > /etc/locale.gen && locale-gen && export LANG=fr_FR.UTF-8 && echo "archlinux" > /etc/hostname && ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime && mkinitcpio -p linux-lts && grub-install --no-floppy --recheck /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg && passwd root && pacman -Syy networkmanager && systemctl enable NetworkManager && exit && umount -R /mnt && reboot
