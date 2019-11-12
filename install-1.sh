27d11ef539f6b916fb16093cf80ef8059e0bc45a
zourit77 <zourit77@free.fr>
2019-11-12 12:47:29 +0100

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -7,8 +7,7 @@ pacman -Syu openssh && systemctl start sshd
 &&
 passwd root
 &&
-ip addr #pour connaître l'adresse ip 
-dhclient #si besoin pour relancer les requêtes DHCP (utiliser la commande "wifi-menu" pour une connexion wifi) 
+hostname -i
 
 
 
